3b2ce5bc4e49cc868bb4dfab6f3ab9b3f0e97b74
Melissa Carbone <mcarbone1@gmail.com>
2013-12-22 12:34:05 -0500

    add factory girl to dog spec

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -6,4 +6,18 @@ describe Dog do
 
   it { should have_many(:owners) }
 
+  it 'can have more than one owner' do
+
+    owners = FactoryGirl.create_list(:owner, 3)
+    dog = FactoryGirl.create(:dog)
+
+     owners.each do |owner|
+     FactoryGirl.create(:dog_owner, owner: owner, dog: dog)
+    end
+
+    owners.each do |owner|
+    expect(dog.owners.include?(owner)).to be_true
+    end
+  end
+
 end
